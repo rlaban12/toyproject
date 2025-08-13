c119bc723a16c658a1ececf2dde8497cab8d9f1b
rlaban12 <rlaeogus9786@gmail.com>
2025-08-13 12:30:05 +0900

    20250813_1230 commit

--- src/test/java/com/spring/toyproject/repository/UserRepositoryTest.java
+++ src/test/java/com/spring/toyproject/repository/UserRepositoryTest.java
@@ -7,13 +7,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest   // DB 테스트를 위한 간소설정 (속도가 @SpringBootTest에 비해 월등히 빠름)
-@ActiveProfiles("test") // application-test.yml을 읽어라
+@SpringBootTest  // DB 테스트를 위한 간소설정 (속도가 @SpringBootTest에 비해 월등히 빠름)
+//@ActiveProfiles("test") // application-test.yml을 읽어라
+@Transactional
 class UserRepositoryTest {
 
     @Autowired
@@ -22,8 +25,8 @@ class UserRepositoryTest {
     @BeforeEach
     void setUp() {
         User user = User.builder()
-                .username("testUser")
-                .email("test@example.com")
+                .username("testUser1")
+                .email("test1@example.com")
                 .password("password123")
                 .build();
 
@@ -35,19 +38,19 @@ class UserRepositoryTest {
     @DisplayName("사용자명으로 조회 테스트")
     void findByUsernameTest() {
         //given
-        String username = "testUser";
+        String username = "testUser1";
         //when
         User foundUser = userRepository.findByUsername(username).orElseThrow();
         //then
         assertNotNull(foundUser);
-        assertEquals("test@example.com", foundUser.getEmail());
+        assertEquals("test1@example.com", foundUser.getEmail());
     }
 
     @Test
     @DisplayName("이메일로 조회 테스트")
     void findByEmailTest() {
         //given
-        String email = "test@example.com";
+        String email = "test1@example.com";
         //when
         User foundUser = userRepository.findByEmail(email).orElseThrow();
 
@@ -56,7 +59,7 @@ class UserRepositoryTest {
         assertThat(foundUser).isNotNull(); // JUnit4
 
 //        assertEquals("testUser", foundUser.getUsername()); // JUnit5
-        assertThat(foundUser.getUsername()).isEqualTo("testUser"); // JUnit4
+        assertThat(foundUser.getUsername()).isEqualTo("testUser1"); // JUnit4
     }
 
 
@@ -76,7 +79,7 @@ class UserRepositoryTest {
     @DisplayName("이메일 중복확인 테스트")
     void existsEmailTest() {
         //given
-        String email = "test@example.com";
+        String email = "test1@example.com";
         //when
         boolean flag = userRepository.existsByEmail(email);
         //then
